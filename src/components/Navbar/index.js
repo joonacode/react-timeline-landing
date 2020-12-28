93f62f4ee67c3fc4f548b8983e0c2850aba3eec1
joonacode <joonacode@gmail.com>
2020-12-28 21:24:55 +0700

    update navbar & sidebar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -12,33 +12,21 @@ const Navbar = () => {
         <div className='flex items-center justify-between'>
           <div className='flex items-center'>
             <div>
-              <img src={Logo} className='h-10 w-10' alt='logo' />
+              <img src={Logo} className='h-10 w-10 cursor-pointer' alt='logo' />
             </div>
             <div className='lg:ml-20 md:ml-10  items-center md:flex hidden'>
-              <a
-                href='/'
-                className='mr-10 text-sm text-gray-800 hover:opacity-90'
-              >
+              <span className='mr-10 text-sm cursor-pointer text-gray-800 hover:opacity-90'>
                 HOME
-              </a>
-              <a
-                href='/'
-                className='mr-10 text-sm text-gray-800 hover:opacity-90 opacity-50'
-              >
+              </span>
+              <span className='mr-10 text-sm cursor-pointer text-gray-800 hover:opacity-90 opacity-50'>
                 PLANS
-              </a>
-              <a
-                href='/'
-                className='mr-10 text-sm text-gray-800 hover:opacity-90 opacity-50'
-              >
+              </span>
+              <span className='mr-10 text-sm cursor-pointer text-gray-800 hover:opacity-90 opacity-50'>
                 FEATURES
-              </a>
-              <a
-                href='/'
-                className='mr-10 text-sm text-gray-800 hover:opacity-90 opacity-50'
-              >
+              </span>
+              <span className='mr-10 text-sm cursor-pointer text-gray-800 hover:opacity-90 opacity-50'>
                 CONTACT
-              </a>
+              </span>
             </div>
           </div>
           <button className='md:block hidden bg-purple-700 hover:bg-purple-500 z-10 text-white rounded-full px-8 py-2 font-bold focus:outline-none'>
